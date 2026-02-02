840165795bb4ea39a4b87f3e7f45344feb5d8d0f
akhioffice1120-dotcom <akhioffice1120@gmail.com>
2026-02-02 14:06:07 +0600

    Update settings.py

--- arch_design_project/settings.py
+++ arch_design_project/settings.py
@@ -258,7 +258,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
+    "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+},
 }
 # MEDIA_URL = "/media/"
