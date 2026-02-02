10fbcfb55e56749d0cf2fc8d775ff9ba8c97187b
akhioffice1120-dotcom <akhioffice1120@gmail.com>
2026-02-02 11:50:23 +0600

    Update settings.py

--- arch_design_project/settings.py
+++ arch_design_project/settings.py
@@ -241,3 +241,15 @@ LOGGING = {
         "level": LOG_LEVEL,
     },
 }
+
+INSTALLED_APPS += ["cloudinary", "cloudinary_storage"]
+
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": config("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": config("CLOUDINARY_API_KEY"),
+    "API_SECRET": config("CLOUDINARY_API_SECRET"),
+}
+
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+MEDIA_URL = "/media/"  
+
